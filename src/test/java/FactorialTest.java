fd3124eee922a061934190f8769d27ccdd85d83a
Aleksandr Olenskii <olen-aleksandr@yande.ru>
2024-06-27 00:29:51 +0400

    Fix  task 12 testng

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -1,24 +1,22 @@
 package src.test.java;
-
-import org.junit.jupiter.api.Test;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 import src.main.java.org.olenskii.Factorial;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FactorialTest {
     @Test
-    void testCalculateFactorial_Zero() {
-        assertEquals(1, Factorial.calculateFactorial(0));
+    public void testCalculateFactorial_Zero() {
+        Assert.assertEquals(1, Factorial.calculateFactorial(0));
     }
 
     @Test
-    void testCalculateFactorial_Positive() {
-        assertEquals(120, Factorial.calculateFactorial(5));
+    public void testCalculateFactorial_Positive() {
+        Assert.assertEquals(120, Factorial.calculateFactorial(5));
     }
 
-    @Test
-    void testCalculateFactorial_Negative() {
-        assertThrows(IllegalArgumentException.class, () -> Factorial.calculateFactorial(-1));
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testCalculateFactorial_Negative() {
+        Factorial.calculateFactorial(-1);
     }
+
 }
